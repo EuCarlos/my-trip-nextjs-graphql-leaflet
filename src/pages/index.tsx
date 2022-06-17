c213d167888b5f9c721706d777ff3b2bd420ac9a
Carlos Alve <carlosalves369m@hotmail.com>
2022-06-17 13:14:30 -0300

    :construction: create about page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
 // import Map from "../components/Map"
+import LinkWrapper from "components/LinkWrapper"
+import { InfoOutline } from "@styled-icons/evaicons-outline"
+
 import dynamic from "next/dynamic"
 
 const Map = dynamic(() => import('../components/Map'), { ssr: false })
 
 export default function Home() {
-  return <Map />
+  return <>
+    <LinkWrapper href="/about">
+      <InfoOutline size={32} aria-label="About" />
+    </LinkWrapper>
+    <Map />
+  </>
 }
