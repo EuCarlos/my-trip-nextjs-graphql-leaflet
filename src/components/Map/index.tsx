328790a94fda8386e0817ae18013ca5b6a88bada
Carlos Alve <carlosalves369m@hotmail.com>
2022-06-19 13:53:49 -0300

    :zap: define boundary to avoid map repetitions

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -19,7 +19,16 @@ const Map = ({ places }: MapProps) => {
   const router = useRouter()
 
   return (
-    <MapContainer center={[0, 0]} zoom={3} style={{ height: '100%', width: '100%'}}>
+    <MapContainer
+      center={[0, 0]}
+      zoom={3}
+      style={{ height: '100%', width: '100%'}}
+      minZoom={3}
+      maxBounds={[
+        [-100, 100],
+        [100, -100]
+      ]}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
